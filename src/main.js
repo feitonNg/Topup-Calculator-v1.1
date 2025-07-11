828684365a4075030222a6175244c3ae5e72778d
feitonNg <honliang99@hotmail.com>
2025-07-11 16:36:14 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
 function createWindow() {
-  const win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadFile('src/index.html');
+  const win = new BrowserWindow({
+    width: 800,
+    height: 800,
+    webPreferences: {
+      contextIsolation: false
+    }
+  });
+  win.loadFile(path.join(__dirname, 'index.html'));
 }
+
 app.whenReady().then(createWindow);
